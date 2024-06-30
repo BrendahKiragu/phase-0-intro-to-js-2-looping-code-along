4fa80747e43ceb6b2dd8328cd6f89bc366faa353
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-06-30 20:14:48 +0300

    creates a for loop with array

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// creates an array of names list
+const names =  ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards (names){
+  //creates a new empty array to hold the "thank you" messages
+  let cardsMessage = [];
+
+  //iterates through the input array
+  for (let i=0; i< names.length; i++) {
+  
+  //creates the `thank you..` message using string interpolation
+    let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+
+    //adds the message to the new array
+    cardsMessage.push(message)
+  }
+  return cardsMessage;
+}
+
+console.log(writeCards(names));
